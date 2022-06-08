2ee318412c764fdfbfb696bdc0cdeee086b887ba
Davide <franc.davi00@gmail.com>
2022-06-08 16:27:12 +0200

    header creato

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Header/>
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,117 @@
+<template>
+    <header>
+        <div class="container">
+            <a href="">
+                <img src="../assets/img/dc-logo.png" alt="logo dc comics">
+            </a>
+
+            <div class="space-left">
+                <nav>
+                    <ul>
+                        <li v-for="(link, index) in links" :key="index">
+                            <a :href="link.url">{{link.text}}</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'myHeader',
+  data(){
+      return{
+          links : [
+              {
+                text :"CHARACTERS",
+                url : "#"
+              },
+
+            {
+                text :"COMICS",
+                url : "#"
+            },
+             {
+                text :"MOVIES",
+                url : "#"
+              },
+
+                {
+                text :"TV",
+                url : "#"
+              },
+
+                {
+                text :"GAMES",
+                url : "#"
+              },
+
+                {
+                text :"COLLECTIBILES",
+                url : "#"
+              },
+
+                {
+                text :"VIDEOS",
+                url : "#"
+              },
+
+                {
+                text :"FANS",
+                url : "#"
+              },
+
+                {
+                text :"NEWS",
+                url : "#"
+              },
+
+                {
+                text :"SHOP",
+                url : "#"
+              },
+
+          ]
+      }
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header{
+    margin-top: 2%;
+}
+
+.container{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+li{
+    display: inline;
+    list-style-type: none;
+    margin-left: 2%;
+}
+
+.space-left{
+    margin-right: 10%;
+}
+
+a{
+    color: black;
+    text-decoration: none;
+}
+
+a:hover{
+    color: rgb(14, 17, 214);
+    border-bottom: 3px solid blue;
+    padding-bottom: 5%;
+}
+
+</style>
